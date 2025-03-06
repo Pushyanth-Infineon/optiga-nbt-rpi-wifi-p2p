c63c4bf4f7ea88fe8ed896e2139e8441e4c5ee9a
KamathamPush <Pushyanth.Kamatham@infineon.com>
2025-03-06 15:01:21 +0530

    Updated main.c to write WiFi Connection handover message to NDEF file.

--- source/main.c
+++ source/main.c
@@ -6,6 +6,11 @@
 #include "infineon/i2c-rpi.h"
 #include "infineon/logger-printf.h"
 
+#include "utilities/nbt-utilities.h"
+
+/* Required for POSIX threads */
+#include <pthread.h>
+
 /* Required for I2C */
 #include <unistd.h>
 #include <fcntl.h>
@@ -23,6 +28,225 @@
 #define RPI_I2C_INIT_FAIL   (-2)
 #define OPTIGA_NBT_ERROR    (-3)
 
+/**
+ * \brief Skeleton for WiFi connection handover message.
+ */
+// clang-format off
+static uint8_t WIFI_CONNECTION_HANDOVER_MESSAGE[] = 
+{   // NDEF record header. MB=1b, ME=0b, CF=0b, SR=1b, IL=0b, TNF=001b
+    0x91,
+    // Record Type length (2 octets)
+    0x02,
+    // Record Type Length: 10 octets.
+    0x0a,
+    // Record Type: "Hs"
+    0x48, 0x73,
+    // Version Number: Major = 1, Minor = 3
+    0x13, 
+    // NDEF Record Header: MB=1b, ME=1b, CF=0b, SR=1b, IL=0b, TNF=001b
+    0xd1,
+    // Record Type Length: 2 octets
+    0x02,
+    // Payload Length: 4 octets
+    0x04,
+    // Record Type: "ac" (Alternate Carrier) records
+    0x61, 0x63,
+    // Carrier Flags: CPS=1, "active"
+    0x01,
+    // Carrier Data Reference Length: 1 octet
+    0x01,
+    // Carrier Data Reference: "0"
+    0x30,
+    // Auxiliary Data Reference Count: 0
+    0x0,
+    // NDEF Record Header: MB=0b, ME=1b, CF=0b, SR=1b, IL=1b, TNF=010b
+    0x5a,
+    // Record Type Length: 23 octets
+    0x17,
+    // Payload length: 87 octets
+    0x57,
+    // Id Length: 1 octet
+    0x01,
+    // Record Type Name: "application/vnd.wfa.wsc"
+    0x61, 0x70, 0x70, 0x6c, 0x69, 0x63, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x2f, 0x76, 0x6e, 0x64, 0x2e, 0x77, 0x66, 0x61, 0x2e, 0x77, 0x73, 0x63,
+    // Id: "0"
+    0x30,
+    // MAC address (1020)
+    0x10, 0x20, 0x00,
+    // Length: 6 octets
+    0x06, 
+    // MAC address: "123456"
+    0x31, 0x32, 0x33, 0x34, 0x35, 0x36,
+    // Out Of Band Device Password: 2 octets
+    0x10, 0x2c,
+    // Payload length: 2 octets
+    0x00, 0x26, 
+    // Public key hash: 20 octets
+    0x95, 0xc8, 0xd2, 0x7a, 0xa8, 0x94, 0x63, 0x38, 0x71, 0x3a, 0x8d, 0xe9, 0x68, 0xf8, 0xde, 0x7a, 0x72, 0x33, 0x8c, 0xef,
+    // Record length: 2 octets
+    0x00, 0x10,
+    // Password: 16 octets
+    0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66,
+    // Record Type: "RF Bands" - 2 octets
+    0x10, 0x3c,
+    // Record length: 2 octets
+    0x00, 0x01, 
+    // 2.4 GHz (0x01)
+    0x01, 
+    // Record type: "SSID"
+    0x10, 0x45, 
+    // Record length: 2 octets
+    0x0, 0x0a, 
+    // SSID: "DIRECT-Rpi" - 10 octets
+    0x44, 0x49, 0x52, 0x45, 0x43, 0x54, 0x2d, 0x52, 0x70, 0x69, 
+    // Record type: "Vendor extension"
+    0x10, 0x49, 
+    // Record length: 2 octets
+    0x00, 0x6, 
+    // WFA Vendor Extension ()
+    0x00, 0x37, 0x2a,
+    
+    0x00, 0x01, 0x20
+};
+// clang-format on
+
+/** GP T=1' I2C protocol - Raspberry PI */
+// Protocol to handle the GP T=1' I2C protocol communication with tag
+ifx_protocol_t gp_i2c_protocol;
+
+// Protocol to handle communication with Raspberry PI I2C driver
+ifx_protocol_t driver_adapter;
+/* Initialize protocol driver layer here with I2C implementation.
+Note: Does not work without initialized driver layer for I2C. */
+
+/* Logger object */
+ifx_logger_t logger_implementation;
+
+/**
+ * \brief NBT abstraction.
+ */
+static nbt_cmd_t nbt;
+
+/* I2C file descriptor */
+static int i2c_fd;
+
+
+/**
+ * \brief Configures NBT for Wifi connection handover usecase.
+ * \details Sets file access policies, configures communication interface and writes connection handover skeleton to NDEF file.
+ * \param[in] nbt NBT abstraction for communication.
+ * \return ifx_status_t \c IFX_SUCCESS if successful, any other value in case of error.
+ */
+static ifx_status_t nbt_configure_wifi_connection_handover(nbt_cmd_t *nbt)
+{
+    if (nbt == NULL)
+    {
+        return IFX_ERROR(LIB_NBT_APDU, NBT_SET_CONFIGURATION, IFX_ILLEGAL_ARGUMENT);
+    }
+    const nbt_file_access_policy_t fap_cc = {.file_id = NBT_FILEID_CC,
+                                             .i2c_read_access_condition = NBT_ACCESS_ALWAYS,
+                                             .i2c_write_access_condition = NBT_ACCESS_NEVER,
+                                             .nfc_read_access_condition = NBT_ACCESS_ALWAYS,
+                                             .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t fap_ndef = {.file_id = NBT_FILEID_NDEF,
+                                               .i2c_read_access_condition = NBT_ACCESS_ALWAYS,
+                                               .i2c_write_access_condition = NBT_ACCESS_ALWAYS,
+                                               .nfc_read_access_condition = NBT_ACCESS_ALWAYS,
+                                               .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t fap_fap = {.file_id = NBT_FILEID_FAP,
+                                              .i2c_read_access_condition = NBT_ACCESS_ALWAYS,
+                                              .i2c_write_access_condition = NBT_ACCESS_ALWAYS,
+                                              .nfc_read_access_condition = NBT_ACCESS_ALWAYS,
+                                              .nfc_write_access_condition = NBT_ACCESS_ALWAYS};
+    const nbt_file_access_policy_t fap_proprietary1 = {.file_id = NBT_FILEID_PROPRIETARY1,
+                                                       .i2c_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .i2c_write_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t fap_proprietary2 = {.file_id = NBT_FILEID_PROPRIETARY2,
+                                                       .i2c_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .i2c_write_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t fap_proprietary3 = {.file_id = NBT_FILEID_PROPRIETARY3,
+                                                       .i2c_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .i2c_write_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t fap_proprietary4 = {.file_id = NBT_FILEID_PROPRIETARY4,
+                                                       .i2c_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .i2c_write_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_read_access_condition = NBT_ACCESS_NEVER,
+                                                       .nfc_write_access_condition = NBT_ACCESS_NEVER};
+    const nbt_file_access_policy_t *faps[] = {&fap_cc, &fap_ndef, &fap_fap, &fap_proprietary1, &fap_proprietary2, &fap_proprietary3, &fap_proprietary4};
+    const struct nbt_configuration configuration = {.fap = (nbt_file_access_policy_t **) faps,
+                                                    .fap_len = sizeof(faps) / sizeof(struct nbt_configuration *),
+                                                    .communication_interface = NBT_COMM_INTF_NFC_ENABLED_I2C_ENABLED,
+                                                    .irq_function = NBT_GPIO_FUNCTION_DISABLED};
+    ifx_status_t status = nbt_configure(nbt, &configuration);
+    if (ifx_error_check(status))
+    {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_FATAL, "Could not confgure NBT for connection handover usecase.");
+        return status;
+    }
+}
+
+
+/**
+ * \brief posix thread to write the Wifi P2P connection handover Select data to NDEF file
+ *
+ * \details
+ *   * Opens communication channel to NBT.
+ *   * Configures NBT for asynchronous data transfer usecase.
+ *   * Reads configuration from NBT proprietary file 1.
+ *   * Sets LED state depending on configuration.
+ *   * Writes LED state to NBT proprietary file 2.
+ *   * Waits for button interrupt and starts loop again.
+ *
+ * \see nbt_configure_adt()
+ */
+void* nbt_write_ndef(void *arg)
+{
+    // Activate communication channel to NBT
+    uint8_t *atpo = NULL;
+    size_t atpo_len = 0U;
+    ifx_status_t status = ifx_protocol_activate(&gp_i2c_protocol, &atpo, &atpo_len);
+    if (ifx_error_check(status))
+    {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_FATAL, "Could not open communication channel to NBT");
+        goto exit;
+    }
+
+    // Set NBT to Connection handover configuration
+    status = nbt_configure_wifi_connection_handover(&nbt);
+    if (ifx_error_check(status))
+    {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_FATAL, "Could not set NBT to WiFi Connection handover configuration");
+        goto exit;
+    }
+
+    // Use NBT command abstraction
+    status = nbt_select_nbt_application(&nbt);
+    if (ifx_error_check(status))
+    {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_ERROR, "Could not select NBT application");
+        goto exit;
+    }
+
+    // Write the NDEF message
+    status = nbt_write_file(&nbt, NBT_FILEID_NDEF, 0U, WIFI_CONNECTION_HANDOVER_MESSAGE, sizeof(WIFI_CONNECTION_HANDOVER_MESSAGE));
+    if (ifx_error_check(status))
+    {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_ERROR, "Could not write NBT NDEF file");
+        goto exit;
+    }
+
+exit:
+    *(int *)(arg) = status;
+    return arg;
+}
+
+
 int main()
 {
     /** GP T=1' I2C protocol - PSoC&trade;6 Host MCU */
@@ -34,14 +258,15 @@ int main()
     /* Initialize protocol driver layer here with I2C implementation.
     Note: Does not work without initialized driver layer for I2C. */
 
-    /* Logger object */
-    ifx_logger_t logger_implementation;
-
     // code placeholder
     ifx_status_t status;
 
     /* I2C file descriptor */
     int i2c_fd;
+
+    /* Pthread ID */
+    pthread_t ptid; 
+    void *pthread_status = &status;
 
     /* Initialize logging */
     status = logger_printf_initialize(ifx_logger_default);
@@ -88,47 +313,26 @@ int main()
         goto cleanup;
     }
 
-    // Select Type 4 Tag application
-    uint8_t select_app[] = {0x00, 0xa4, 0x04, 0x00, 0x07, 0xd2, 0x76, 0x00, 0x00, 0x85, 0x01, 0x01, 0x00};
-
-    // Select application with File ID: E1A1 and length = 1:
-    uint8_t select_aid[] = {0x00, 0xa4, 0x00, 0x0c, 0x02, 0xe1, 0xa1, 0x00};
-
-    // Write one byte (0xEE)
-    uint8_t write_file[] = {0x00, 0xd6, 0x00, 0x00, 0x01, 0xEE};
-
-    // Read back: 
-    uint8_t read_file[] = {0x00, 0xb0, 0x00, 0x00, 0x01};
-
-    // Exchange data with the secure element
-    uint8_t *response = malloc(30);
-    size_t response_len = 0u;
-    status = ifx_protocol_transceive(&gp_i2c_protocol, select_app, sizeof(select_app), &response, &response_len);
-    if (response != NULL) free(response);
-    if (status != IFX_SUCCESS)
+    // NBT command abstraction
+    status = nbt_initialize(&nbt, &gp_i2c_protocol, ifx_logger_default);
+    if (ifx_error_check(status))
     {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_ERROR, "Could not initialize NBT abstraction");
         goto cleanup;
     }
 
-    status = ifx_protocol_transceive(&gp_i2c_protocol, select_aid, sizeof(select_aid), &response, &response_len);
-    if (response != NULL) free(response);
-    if (status != IFX_SUCCESS)
+    /* Create a thread to perform nbt_write_ndef function */
+    if (0 != pthread_create(&ptid, NULL, nbt_write_ndef, pthread_status))
     {
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_ERROR, "Could not create POSIX thread");
         goto cleanup;
     }
 
-    status = ifx_protocol_transceive(&gp_i2c_protocol, write_file, sizeof(write_file), &response, &response_len);
-    if (response != NULL) free(response);
-    if (status != IFX_SUCCESS)
+    /* Wait till thread completion. Should not come here */
+    pthread_join(ptid, &pthread_status);
+    if (*(int*)pthread_status)
     {
-        goto cleanup;
-    }
-
-    status = ifx_protocol_transceive(&gp_i2c_protocol, read_file, sizeof(read_file), &response, &response_len);
-    if (response != NULL) free(response);
-    if (status != IFX_SUCCESS)
-    {
-        goto cleanup;
+        ifx_logger_log(ifx_logger_default, LOG_TAG, IFX_LOG_ERROR, "POSIX thread: nbt_write_ndef failed with: (%d)", *(int *)pthread_status);
     }
 
 cleanup:
@@ -136,8 +340,10 @@ cleanup:
     // Perform cleanup of full protocol stack
     ifx_protocol_destroy(&gp_i2c_protocol);
 
+    // Destroy NBT command abstraction
+    nbt_destroy(&nbt);
 exit:
-    // Close the File 
+    // Close the File descriptor
     close(i2c_fd);
 
 ret:
